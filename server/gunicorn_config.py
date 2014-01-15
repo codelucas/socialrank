6a7c238938f4727c116de85a0f03f505afa20bde
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-01-15 12:24:33 -0800

    fixed bug where gunicorn spawns new process so supervisor now works, block illiegal hostnames on server (nginx) level

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -3,4 +3,5 @@ pythonpath = '/home/lucas/www/socialrank.codelucas.com/socialrank-env/socialrank
 bind = '127.0.0.1:8080'
 workers = 1
 user = 'lucas'
-accesslog = '/home/lucas/logs/socialrank.codelucas.com/gunicorn.log'
+accesslog = '/home/lucas/logs/socialrank.codelucas.com/gunicorn-access.log'
+errorlog = '/home/lucas/logs/socialrank.codelucas.com/gunicorn-error.log'
